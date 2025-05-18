77e743bcd548ab70857eaaf57672e96ba429fb50
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-05-18 12:44:06 +0100

    chore: printing

--- cli/src/actions/run.rs
+++ cli/src/actions/run.rs
@@ -1,14 +1,66 @@
 use std::{path::PathBuf, process::exit};
 
 use colored::Colorize;
+use reqwest::{Client, Version};
 use rustle_api::executor::runner::{Runner, ScriptEngine};
 use tracing::error;
 
 use super::utils::{quiet_or_print, resolve_path};
 use crate::ds::RunMode;
 
-async fn run_request(runner: &mut Runner, name: String, quiet: bool) {
-    let client = reqwest::Client::new();
+async fn print_response(response: reqwest::Response, quiet: bool) {
+    // Print status line
+    let status = response.status();
+    let status_color = if status.is_success() {
+        status.to_string().green()
+    } else {
+        status.to_string().red()
+    };
+
+    let http_version = match response.version() {
+        Version::HTTP_09 => "HTTP/0.9",
+        Version::HTTP_10 => "HTTP/1.0",
+        Version::HTTP_11 => "HTTP/1.1",
+        Version::HTTP_2 => "HTTP/2",
+        Version::HTTP_3 => "HTTP/3",
+        _ => "HTTP/1.1",
+    };
+
+    quiet_or_print(format!(" * {} {}", http_version, status_color,), quiet);
+
+    // Print headers
+    for (name, value) in response.headers() {
+        quiet_or_print(
+            format!(
+                " * {}: {}",
+                name.to_string().cyan(),
+                value.to_str().unwrap_or("")
+            ),
+            quiet,
+        );
+    }
+
+    quiet_or_print(format!(""), quiet); // Empty line between headers and body
+
+    // Print body
+    if let Ok(text) = response.text().await {
+        if !text.is_empty() {
+            // Try to parse as JSON for pretty printing
+            if let Ok(json) = serde_json::from_str::<serde_json::Value>(&text) {
+                quiet_or_print(
+                    format!(" * {}", serde_json::to_string_pretty(&json).unwrap()),
+                    quiet,
+                );
+            } else {
+                quiet_or_print(format!(" * {}", text), quiet);
+            }
+        }
+    }
+
+    quiet_or_print(format!(""), quiet); // Empty line after response
+}
+
+async fn base_run_request(runner: &mut Runner, client: &Client, name: String, quiet: bool) {
     let stack = match runner.generate_call_queue(&name) {
         Ok(s) => s,
         Err(e) => {
@@ -26,9 +78,13 @@ async fn run_request(runner: &mut Runner, name: String, quiet: bool) {
             }
         };
 
-        // TODO: display result
-        println!("{:?}", result);
+        print_response(result, quiet).await;
     }
+}
+
+async fn run_request(runner: &mut Runner, name: String, quiet: bool) {
+    let client = reqwest::Client::new();
+    base_run_request(runner, &client, name, quiet).await
 }
 
 async fn run_sequence(runner: &mut Runner, name: String, quiet: bool) {
@@ -46,17 +102,9 @@ async fn run_sequence(runner: &mut Runner, name: String, quiet: bool) {
     for request_seq in stack {
         for request in request_seq {
             let schema = runner.get_request_schema(&request).unwrap();
+
             quiet_or_print(format!("-> REQ {} {}", request.green(), schema.url), quiet);
-
-            let result = match runner.call_request(request, &client).await {
-                Ok(result) => result,
-                Err(e) => {
-                    error!("Error calling request: {}", e);
-                    exit(-1);
-                }
-            };
-
-            // print the respose
+            base_run_request(runner, &client, request, quiet).await;
         }
     }
 }
