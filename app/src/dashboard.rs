15f62da2a50c8e53985885aebe64d59c5dc06a8f
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-06-09 11:06:11 +0100

    clean ui

--- app/src/dashboard.rs
+++ app/src/dashboard.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use dioxus_free_icons::{icons::hi_outline_icons, Icon};
 use rfd::AsyncFileDialog;
 
 use crate::{
@@ -24,31 +25,64 @@ pub fn DashboardHeader() -> Element {
         WmDragArea { class: "py-2 flex items-center",
 
             // env select
-            div { class: "flex-grow pl-28",
+            div { class: "flex-grow pl-28 items-center gap-2 flex",
+                // open button
                 button {
                     class: "bg-gray-100 rounded px-2",
-                    onclick: move |_| {
-                        let mut dispatch = dispatch.clone();
-                        let file_picker = AsyncFileDialog::new()
-                            .set_title(format!("Select {} file", APP_NAME))
-                            .add_filter("Project ", &[FILE_EXTENSION]);
-
-                        spawn(async move {
-                            if let Some(file_path) = file_picker.pick_file().await {
-                                let path = file_path.path();
-                                if path.is_file() {
-                                    if let Some(path) = path.to_str().map(|s| s.to_string()) {
-                                        dispatch(ProjectActions::LoadFile(path)).unwrap();
+                    onclick: {
+                        let dispatch = dispatch.clone();
+                        move |_| {
+                            let mut dispatch = dispatch.clone();
+                            let file_picker = AsyncFileDialog::new()
+                                .set_title(format!("Select {} file", APP_NAME))
+                                .add_filter("Project ", &[FILE_EXTENSION]);
+                            spawn(async move {
+                                if let Some(file_path) = file_picker.pick_file().await {
+                                    let path = file_path.path();
+                                    if path.is_file() {
+                                        if let Some(path) = path.to_str().map(|s| s.to_string()) {
+                                            dispatch(ProjectActions::LoadFile(path)).unwrap();
+                                        }
                                     }
                                 }
-                            }
-                        });
+                            });
+                        }
                     },
 
                     if let Some(path) = project_signal.file_name(false) {
                         span { "{path}" }
-                    }else{
+                    } else {
                         span { "open" }
+                    }
+                }
+
+
+                // create button 
+                button {
+                    class: "bg-gray-100 rounded p-1",
+                    onclick: {
+                        // TODO: this should create a new project
+                        let dispatch = dispatch.clone();
+                        move |_| {
+                            let mut dispatch = dispatch.clone();
+                            let file_picker = AsyncFileDialog::new().set_title("Select folder");
+                            spawn(async move {
+                                if let Some(folder_path) = file_picker.pick_folder().await {
+                                    let path = folder_path.path();
+                                    if path.is_file() {
+                                        if let Some(path) = path.to_str().map(|s| s.to_string()) {
+                                            dispatch(ProjectActions::LoadFile(path)).unwrap();
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    },
+
+                    Icon {
+                        icon: hi_outline_icons::HiPlus,
+                        width: 14,
+                        height: 14,
                     }
                 }
             }
