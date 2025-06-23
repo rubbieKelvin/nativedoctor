80ba8b801b763e43d1ba9ab55d4b9de3ecf2e6f9
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-06-23 14:15:08 +0100

    chore: tauri setup

--- app/src/states/mod.rs
+++
@@ -1,7 +0,0 @@
-// mod app;
-mod project;
-mod toasts;
-
-// pub use app::{ApplicationState, ProjectContentLoadingStatus, RequestLoadingStatus};
-pub use project::{ProjectState, WritableRequest, WritableRequestProperty};
-pub use toasts::{ToastCloseMethod, ToastConfig, ToastState, ToastTitle};
--- app/src/ui/toggle_bar/mod.rs
+++
@@ -1,4 +0,0 @@
-mod toggle_bar;
-pub mod env_toggle_bar;
-pub mod request_toggle_bar;
-pub mod sequence_toggle_bar;
