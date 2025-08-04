e7197703103925e0864f956b2719fdd9e2282aac
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-08-04 14:16:29 +0100

    chore: switching to gpui

--- app/src/session/types.rs
+++
@@ -1,8 +0,0 @@
-use std::path::PathBuf;
-
-/// The user may open the application with `nativedoctor filename.nativedoctor`, ie. opening a project
-/// Or they might want to open a single request file.
-pub enum AppSessionType {
-    Project(PathBuf),
-    SingleRequestFile(PathBuf),
-}
