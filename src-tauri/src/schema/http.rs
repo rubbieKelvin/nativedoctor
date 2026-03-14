0a7fdd92608866a07b9539a4b1a8dfd673e2d28c
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2026-03-14 15:01:07 +0100

    update

--- src-tauri/src/schema/http.rs
+++ src-tauri/src/schema/http.rs
@@ -1,6 +1,5 @@
 /// Key-value pair for params/headers. Used for JSON in/out of read/write_resource_file.
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
 pub struct KeyValuePair {
     pub key: String,
     pub value: String,
@@ -12,18 +11,16 @@ pub struct KeyValuePair {
 
 /// HTTP resource as stored in .request.yaml. Deserialize from YAML, serialize to JSON for frontend.
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
 pub struct HttpResourceFile {
     #[serde(rename = "$schema", skip_serializing_if = "Option::is_none")]
     pub schema: Option<String>,
+    pub id: String,
     #[serde(default)]
     pub folder_id: Option<String>,
     #[serde(default)]
     pub created_at: i64,
     #[serde(default)]
     pub updated_at: Option<i64>,
-    #[serde(rename = "type", default = "default_http_type")]
-    pub resource_type: String,
     #[serde(default)]
     pub name: String,
     #[serde(default)]
@@ -38,8 +35,4 @@ pub struct HttpResourceFile {
     pub body: Option<serde_json::Value>,
     #[serde(default)]
     pub auth: Option<serde_json::Value>,
-}
-
-fn default_http_type() -> String {
-    "http".into()
 }
--- src-tauri/src/schema/sequence.rs
+++ src-tauri/src/schema/sequence.rs
@@ -1,6 +1,5 @@
 /// Sequence node as stored in .sequence.yaml.
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
 pub struct SequenceNode {
     pub id: String,
     pub resource_id: String,
@@ -9,24 +8,18 @@ pub struct SequenceNode {
 
 /// Sequence resource as stored in .sequence.yaml.
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
 pub struct SequenceResourceFile {
     #[serde(rename = "$schema", skip_serializing_if = "Option::is_none")]
     pub schema: Option<String>,
+    pub id: String,
     #[serde(default)]
     pub folder_id: Option<String>,
     #[serde(default)]
     pub created_at: i64,
     #[serde(default)]
     pub updated_at: Option<i64>,
-    #[serde(rename = "type", default = "default_sequence_type")]
-    pub resource_type: String,
     #[serde(default)]
     pub name: Option<String>,
     #[serde(default)]
     pub flow: Option<Vec<SequenceNode>>,
-}
-
-fn default_sequence_type() -> String {
-    "sequence".into()
 }
