4225897d9e07fc42606403a827cc918ac98e9e4b
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2026-04-03 07:03:18 +0100

    chore: shadcn

--- crates/nd-web/frontend/vite.config.ts
+++ crates/nd-web/frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from 'node:url'
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     port: 5173,
     proxy: {
