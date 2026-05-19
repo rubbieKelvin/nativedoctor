abf91dcd172f52816125cddf70308fbcc57853a8
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2026-05-19 14:24:21 +0100

    chore: fixed search input not working

--- crates/nd-app/src/app.rs
+++ crates/nd-app/src/app.rs
@@ -3,13 +3,22 @@ use gpui_component::*;
 
 use crate::ui;
 
-pub struct ND;
+pub struct ND {
+    workspace: Entity<ui::workspace::WorkspaceView>,
+}
+
+impl ND {
+    pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
+        let workspace = cx.new(|cx| ui::workspace::WorkspaceView::new(window, cx));
+        Self { workspace }
+    }
+}
 
 impl Render for ND {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         return div()
             .size_full()
-            .child(ui::workspace::workspace_view(window, cx))
+            .child(self.workspace.clone())
             .children(Root::render_dialog_layer(window, cx))
             .children(Root::render_sheet_layer(window, cx))
             .children(Root::render_notification_layer(window, cx));
@@ -22,6 +31,9 @@ pub fn setup() {
     app.run(move |cx| {
         gpui_component::init(cx);
         crate::theme::init(cx);
+
+        // bring the window to the front
+        cx.activate(true);
 
         // close app on last window close
         cx.on_window_closed(|app, _window| {
@@ -40,6 +52,7 @@ pub fn window_options(cx: &App) -> WindowOptions {
     let bounds = WindowBounds::centered(size(px(1280.), px(800.)), cx);
     return WindowOptions {
         window_bounds: Some(bounds),
+        focus: true,
         ..Default::default()
     };
 }
@@ -49,7 +62,7 @@ pub fn spawn_window(cx: &mut App) {
 
     cx.spawn(async move |cx| {
         cx.open_window(options, |window, cx| {
-            let view = cx.new(|_| ND);
+            let view = cx.new(|cx| ND::new(window, cx));
             cx.new(|cx| Root::new(view, window, cx))
         })
         .expect("Failed to open window");
--- crates/nd-app/src/ui/workspace/mod.rs
+++ crates/nd-app/src/ui/workspace/mod.rs
@@ -1,18 +1,78 @@
 use gpui::*;
-use gpui_component::{ActiveTheme as _, Theme};
+use gpui_component::{input, ActiveTheme, Theme};
 
-pub fn workspace_view<T: 'static>(_window: &mut Window, cx: &mut Context<T>) -> impl IntoElement {
-    let theme = cx.theme().clone();
-
-    div()
-        .flex()
-        .flex_row()
-        .size_full()
-        .bg(theme.background)
-        .text_color(theme.foreground)
-        .child(sidebar(&theme))
+// TODO: implement well
+struct SidebarItem {
+    method: String,
+    label: String,
 }
 
-fn sidebar(theme: &Theme) -> impl IntoElement {
-    return div();
+pub struct WorkspaceView {
+    search_input_state: Entity<input::InputState>,
+}
+
+impl WorkspaceView {
+    pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
+        let search_input_state =
+            cx.new(|cx| input::InputState::new(window, cx).placeholder("Search resources..."));
+
+        cx.subscribe(
+            &search_input_state,
+            move |_this, _entity, ev: &input::InputEvent, cx| {
+                if matches!(ev, input::InputEvent::Change) {
+                    cx.notify();
+                }
+            },
+        )
+        .detach();
+
+        return Self { search_input_state };
+    }
+
+    fn sidebar(&mut self, theme: &Theme) -> impl IntoElement {
+        return div()
+            .w_112()
+            .flex()
+            .flex_col()
+            .gap_2()
+            .border_r(px(1.))
+            .border_color(theme.border)
+            .child(self.sidebar_searchbar(theme));
+    }
+
+    fn sidebar_searchbar(&mut self, theme: &Theme) -> impl IntoElement {
+        return div()
+            .p_3()
+            .border_b(px(1.))
+            .border_color(theme.border)
+            .child(input::Input::new(&self.search_input_state));
+    }
+
+    fn sidebar_items(&mut self, items: Vec<SidebarItem>) -> impl Element {
+        return div().p_3().gap_2().flex().flex_col().children(
+            items
+                .iter()
+                .map(|item| div().child(item.label.clone()))
+                .collect::<Vec<Div>>(),
+        );
+    }
+
+    fn mainpanel(&mut self) -> impl IntoElement {
+        return div().child("Main panel");
+    }
+}
+
+impl Render for WorkspaceView {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let theme = cx.theme().clone();
+
+        return div()
+            .flex()
+            .flex_row()
+            .size_full()
+            .bg(theme.background)
+            .text_color(theme.foreground)
+            .child(self.sidebar(&theme))
+            .child(self.mainpanel());
+    }
 }
