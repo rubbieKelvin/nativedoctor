64d6760dff7b6f13a8ddb2edc129465b6efc894a
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2026-05-22 10:47:48 +0100

    chore: ui update for kv

--- crates/nd-app/src/ui/components/kvinput/row.rs
+++ crates/nd-app/src/ui/components/kvinput/row.rs
@@ -2,6 +2,7 @@ use gpui::*;
 use gpui_component::{
     checkbox::Checkbox,
     input::{Input, InputState},
+    ActiveTheme,
 };
 use uuid::Uuid;
 
@@ -19,10 +20,10 @@ impl KvRowState {
     pub fn new(cx: &mut App, window: &mut Window) -> Self {
         return Self {
             enabled: false,
-            key: cx.new(|cx| InputState::new(window, cx)),
-            value: cx.new(|cx| InputState::new(window, cx)),
+            key: cx.new(|cx| InputState::new(window, cx).placeholder("Key")),
+            value: cx.new(|cx| InputState::new(window, cx).placeholder("Value")),
             is_secret: false,
-            description: cx.new(|cx| InputState::new(window, cx)),
+            description: cx.new(|cx| InputState::new(window, cx).placeholder("Description")),
         };
     }
 }
@@ -43,15 +44,37 @@ impl KvRow {
 impl RenderOnce for KvRow {
     fn render(self, _window: &mut Window, cx: &mut App) -> impl IntoElement {
         let inner_state = self.state.read(cx);
+        let theme = cx.theme();
 
         return div()
             .flex()
             .flex_row()
-            .gap_1()
+            .px_2()
+            .border_b(px(1.))
+            .border_color(theme.border)
             .items_center()
-            .child(Checkbox::new(ElementId::Uuid(Uuid::new_v4())).checked(inner_state.enabled))
-            .child(Input::new(&inner_state.key))
-            .child(Input::new(&inner_state.value))
-            .child(Input::new(&inner_state.description));
+            .child(
+                Checkbox::new(ElementId::Uuid(Uuid::new_v4()))
+                    .checked(inner_state.enabled)
+                    .pr_2(),
+            )
+            .child(
+                Input::new(&inner_state.key)
+                    .appearance(false)
+                    .border_color(theme.border)
+                    .border_l(px(1.)),
+            )
+            .child(
+                Input::new(&inner_state.value)
+                    .appearance(false)
+                    .border_color(theme.border)
+                    .border_l(px(1.)),
+            )
+            .child(
+                Input::new(&inner_state.description)
+                    .appearance(false)
+                    .border_color(theme.border)
+                    .border_l(px(1.)),
+            );
     }
 }
--- crates/nd-app/src/ui/components/request/request_tab_params.rs
+++ crates/nd-app/src/ui/components/request/request_tab_params.rs
@@ -1,5 +1,5 @@
 use gpui::*;
-use gpui_component::{StyledExt, Theme};
+use gpui_component::Theme;
 
 use crate::ui::components::kvinput;
 
